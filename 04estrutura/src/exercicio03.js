5fe67062c04ee00bbea913ba623f34839040b9d5
NICOLAS RODRIGUES DA LUZ <nicolas_r_luz@estudante.sesisenai.org.br>
2026-07-23 19:27:21 -0300

    a

--- 04estrutura/src/exercicio03.js
+++ 04estrutura/src/exercicio03.js
@@ -1,14 +1,19 @@
 import leia from 'readline-sync'
-import { pedirNumeros } from '../utils/pedirNumeros';
+import { pedirNumeros } from "../utils/pedirNumeros.js";
+
+export function exercicio03(){
 
 var nm = [];
 
-var numeros = pedirNumeros()
+var numeros = pedirNumeros(5)
 
-for (var i = 0; i < nm.length; i++){
-if (nm[i] % 2 === 0){
-    console.log(nm[i])
+for (var i = 0; i < numeros.length; i++){
+if (numeros[i] % 2 === 0){
+    nm[i] = numeros[i]
 }
 
-
+console.log(nm[i])
 }
+}
+
+exercicio03()
